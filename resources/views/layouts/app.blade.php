5155fd215c6ad3e10e0da3f035fd99ee80e61313
furis <yukannahinoishi@gmail.com>
2026-05-23 16:37:22 +0700

    fix logo login and tittle tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Cloth Management - @yield('title')</title>
+ <title>@yield('title') | Xyra.id</title>
  <link rel="icon" type="image/png" href="{{ asset('Resource/xyra_logo.png') }}">
  @vite(['resources/css/app.css', 'resources/js/app.js'])
  <style>
